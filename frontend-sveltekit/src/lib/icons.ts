b710f679c6f484984093e210ecbfcb766f596ba4
bbtgnn <bbt.gnn@gmail.com>
2022-01-10 17:46:06 +0100

    Update icons.ts

--- frontend-sveltekit/src/lib/icons.ts
+++ frontend-sveltekit/src/lib/icons.ts
@@ -2,6 +2,8 @@ import Password24 from 'carbon-icons-svelte/lib/Password24';
 import Email24 from 'carbon-icons-svelte/lib/Email24';
 import User24 from 'carbon-icons-svelte/lib/User24';
 import WarningAltFilled16 from 'carbon-icons-svelte/lib/WarningAltFilled16';
+import WarningAltFilled24 from 'carbon-icons-svelte/lib/WarningAltFilled24';
+import Close24 from 'carbon-icons-svelte/lib/Close24';
 
 export const icons = {
 	fields: {
@@ -9,5 +11,7 @@ export const icons = {
 		email: Email24,
 		error: WarningAltFilled16,
 		username: User24
-	}
+	},
+	close: Close24,
+	alert: WarningAltFilled24
 };
