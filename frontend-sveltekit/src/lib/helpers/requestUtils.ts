745a69c128f123f899cdc1de051b6890b98e2e9b
bbtgnn <bbt.gnn@gmail.com>
2022-01-13 10:36:49 +0100

    updated post function error handling

--- frontend-sveltekit/src/lib/helpers/requestUtils.ts
+++ frontend-sveltekit/src/lib/helpers/requestUtils.ts
@@ -29,22 +29,24 @@ export async function post(
 
 		// If the response is not okay
 		if (!res.ok) {
-			// We extract the error message
+			// We have to collect the error message
 			let errorMessage;
 
 			// It can be a generic 404 not found
 			if (res.status == 404) {
 				errorMessage = res.status;
 			}
-
-			// Or a Strapi messsage:
-			// - The first one is strapi specific:
+			// Otherwise we have to extract the message from the object:
+			// - The first way is strapi specific:
 			//   https://strapi.io/blog/how-to-create-a-blog-with-svelte-kit-strapi
-			// - The second one is the generic
+			// - The second and third ones are more generic
 			else {
 				const data = await res.json();
 				errorMessage =
-					data?.message?.[0]?.messages?.[0]?.message || data?.message;
+					data?.message?.[0]?.messages?.[0]?.message ||
+					data?.error?.message ||
+					data?.message ||
+					'Unknown error';
 			}
 
 			// Then we throw the error
