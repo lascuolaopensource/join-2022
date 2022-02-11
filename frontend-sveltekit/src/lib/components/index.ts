ba74e9712dc96369787757580b22eb1203212f96
bbtgnn <bbt.gnn@gmail.com>
2022-02-11 15:53:36 +0100

    removed old form components

--- frontend-sveltekit/src/lib/components/index.ts
+++ frontend-sveltekit/src/lib/components/index.ts
@@ -1,10 +1,6 @@
 import Button from './button.svelte';
 import CardCorso from './cardCorso.svelte';
-import Form from './form.svelte';
-import FormError from './formError.svelte';
-import FormGroup from './formGroup.svelte';
 import IconButton from './iconButton.svelte';
-import InputText from './inputText.svelte';
 import Loading from './loading.svelte';
 import LogoutButton from './logoutButton.svelte';
 import NavbarItem from './navbarItem.svelte';
@@ -16,11 +12,7 @@ import Spinner from './spinner.svelte';
 export {
 	Button,
 	CardCorso,
-	Form,
-	FormError,
-	FormGroup,
 	IconButton,
-	InputText,
 	Loading,
 	LogoutButton,
 	NavbarItem,
