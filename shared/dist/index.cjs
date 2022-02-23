2ed176fbf001526232ac2e63c7dd581fd5811ce7
bbtgnn <bbt.gnn@gmail.com>
2022-02-23 21:47:08 +0100

    s: rebuild

--- shared/dist/index.cjs
+++ shared/dist/index.cjs
@@ -1,21 +1,21 @@
 var yup = require('yup');
 
 function _interopNamespace(e) {
-  if (e && e.__esModule) return e;
-  var n = Object.create(null);
-  if (e) {
-    Object.keys(e).forEach(function (k) {
-      if (k !== 'default') {
-        var d = Object.getOwnPropertyDescriptor(e, k);
-        Object.defineProperty(n, k, d.get ? d : {
-          enumerable: true,
-          get: function () { return e[k]; }
+    if (e && e.__esModule) return e;
+    var n = Object.create(null);
+    if (e) {
+        Object.keys(e).forEach(function (k) {
+            if (k !== 'default') {
+                var d = Object.getOwnPropertyDescriptor(e, k);
+                Object.defineProperty(n, k, d.get ? d : {
+                    enumerable: true,
+                    get: function () { return e[k]; }
+                });
+            }
         });
-      }
-    });
-  }
-  n["default"] = e;
-  return n;
+    }
+    n["default"] = e;
+    return n;
 }
 
 var yup__namespace = /*#__PURE__*/_interopNamespace(yup);
@@ -24,6 +24,9 @@ var re = {
   url: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/,
   cf: /^(?:[A-Z][AEIOU][AEIOUX]|[AEIOU]X{2}|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$/
 };
+var urlSchema = yup__namespace.string().matches(re.url);
+var cfSchema = yup__namespace.string().matches(re.cf);
+var emailSchema = yup__namespace.string().email();
 function thenReq(value) {
   return {
     is: value,
@@ -48,11 +51,26 @@ function thenNull(value) {
 }
 
 /**
+ * LoginEmail
+ */
+
+var leValues = {
+  email: ""
+};
+var leSchema = yup__namespace.object({
+  email: emailSchema.required()
+});
+
+var loginEmail = {
+    __proto__: null,
+    leValues: leValues,
+    leSchema: leSchema
+};
+
+/**
  * Util
  */
 
-var urlVal = yup__namespace.string().matches(re.url);
-var cfVal = yup__namespace.string().matches(re.cf);
 /**
  * User
  */
@@ -78,9 +96,9 @@ var evaluationVal = yup__namespace.object({
   letterNeeded: yup__namespace["boolean"]().required(),
   letter: yup__namespace.string().when("letterNeeded", thenReq(true)),
   portfolioNeeded: yup__namespace["boolean"]().required(),
-  portfolio: urlVal.when("portfolioNeeded", thenReq(true)),
+  portfolio: urlSchema.when("portfolioNeeded", thenReq(true)),
   cvNeeded: yup__namespace["boolean"]().required(),
-  cv: urlVal.when("cvNeeded", thenReq(true))
+  cv: urlSchema.when("cvNeeded", thenReq(true))
 });
 /**
  * Address
@@ -101,13 +119,13 @@ var billingVal = yup__namespace.object({
   billingOption: yup__namespace.string().oneOf(billingOptions).required(),
   // Me
   me: yup__namespace.object({
-    cf: cfVal
+    cf: cfSchema
   }).when("billingOption", thenReq(billingOptions[0])),
   // Persona fisica
   person: yup__namespace.object({
     name: yup__namespace.string().required(),
     surname: yup__namespace.string().required(),
-    cf: cfVal
+    cf: cfSchema
   }).when("billingOption", thenReq(billingOptions[1])),
   // Azienda
   company: yup__namespace.object({
@@ -131,20 +149,41 @@ var enrollVal = yup__namespace.object({
   evaluation: evaluationVal.when("evaluationNeeded", thenReq(true)),
   billingNeeded: yup__namespace["boolean"]().required(),
   billing: billingVal.when("billingNeeded", thenReq(true))
-}).required();
+}).required(); //
 
-var index = {
-  __proto__: null,
-  urlVal: urlVal,
-  cfVal: cfVal,
-  userVal: userVal,
-  phoneVal: phoneVal,
-  evaluationVal: evaluationVal,
-  addressVal: addressVal,
-  billingOptions: billingOptions,
-  billingVal: billingVal,
-  enrollVal: enrollVal
+var index$1 = {
+    __proto__: null,
+    userVal: userVal,
+    phoneVal: phoneVal,
+    evaluationVal: evaluationVal,
+    addressVal: addressVal,
+    billingOptions: billingOptions,
+    billingVal: billingVal,
+    enrollVal: enrollVal,
+    loginEmail: loginEmail
 };
 
-exports.validators = index;
+var PublicationState;
+
+(function (PublicationState) {
+  PublicationState["Live"] = "LIVE";
+  PublicationState["Preview"] = "PREVIEW";
+})(PublicationState || (PublicationState = {}));
+
+var Enum_Enrollment_State;
+
+(function (Enum_Enrollment_State) {
+  Enum_Enrollment_State["Pending"] = "pending";
+  Enum_Enrollment_State["Approved"] = "approved";
+  Enum_Enrollment_State["Rejected"] = "rejected";
+})(Enum_Enrollment_State || (Enum_Enrollment_State = {}));
+
+var index = {
+    __proto__: null,
+    get PublicationState () { return PublicationState; },
+    get Enum_Enrollment_State () { return Enum_Enrollment_State; }
+};
+
+exports.f = index$1;
+exports.t = index;
 //# sourceMappingURL=index.cjs.map
---
+++ shared/dist/validators/loginEmail.d.ts
@@ -0,0 +1,20 @@
+/**
+ * LoginEmail
+ */
+export declare const LEValues: {
+    email: string;
+};
+export declare const LESchema: import("yup/lib/object").OptionalObjectSchema<{
+    email: import("yup/lib/string").RequiredStringSchema<string, import("yup/lib/types").AnyObject>;
+}, import("yup/lib/object").AnyObject, import("yup/lib/object").TypeOfShape<{
+    email: import("yup/lib/string").RequiredStringSchema<string, import("yup/lib/types").AnyObject>;
+}>>;
+export declare type LEBody = typeof LEValues;
+export declare type LEResponse = {
+    email: string;
+    username: string;
+};
+export declare const enum LEErrors {
+    notFound = "LENotFound",
+    badRequest = "LEBadRequest"
+}
