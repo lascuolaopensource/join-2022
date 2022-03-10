87f845eee6fd7baa6e8334f0dc3932c972f1c034
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-03-10 19:53:23 +0100

    fe: organized courses in active and archive

---
+++ frontend-sveltekit/src/routes/inside/courses/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts" context="module">
+	/** @type {import('@sveltejs/kit').Load} */
+	export async function load({ params, fetch, session, stuff }) {
+		return {
+			status: 302,
+			redirect: `/inside/courses/active`
+		};
+	}
+</script>
