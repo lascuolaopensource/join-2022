8dae40c32de1d94c12a3bbe63ca0387dee4f4ba3
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-03-10 19:55:19 +0100

    fe: removed gql client and sdk
    Sia il client gql che l'sdk con i tipi sono stati rimossi, in quanto i tipi sono stati spostati in shared, mentre per il momento il client gql non verrà usato, in quanto non funziona dentro "load".

--- frontend-sveltekit/src/lib/requestUtils/index.ts
+++ frontend-sveltekit/src/lib/requestUtils/index.ts
@@ -1,7 +1,4 @@
-export * from './createGQLClient';
 export * from './authorizationHeader';
-export * as sdk from './sdk';
-
 export { baseUrl } from './baseUrl';
 export { request } from './request';
 export { req } from './requestManager';
