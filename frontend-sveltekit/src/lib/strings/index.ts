a43911aba1e251d69a1ba40d6574ad526405e41b
bbtgnn <bbt.gnn@gmail.com>
2022-03-10 12:25:25 +0100

    fe: created strings object

---
+++ frontend-sveltekit/src/lib/strings/index.ts
@@ -0,0 +1,20 @@
+export const s = {
+	enroll: {
+		info: {
+			title: `Alcune note prima di iscriverti`,
+			payment: `Questo corso è a pagamento.
+                Per questioni gestionali ti chiediamo di pagare in anticipo il corso.
+                Se non dovesse partire, ti rimborseremo immediatamente l'intero importo.`,
+			letter: `Per iscriverti a questo corso è richiesta una lettera motivazionale.
+                Se vuoi prepararla con calma prima di compilare il form di iscrizione noi ti aspettiamo qui :)`,
+			accountCreation: `Per questioni gestionali, al termine dell'iscrizione verrà creato automaticamente un account Join.
+                Riceverai una mail di conferma dell'avvenuta iscrizione.
+                Se non sei interessato ad avere un account, ignora la mail:
+                l'account verrà eliminato automaticamente appena la tua iscrizione al corso verrà confermata.`
+		},
+		button: {
+			payment: 'Vai al pagamento',
+			enroll: 'Iscriviti!'
+		}
+	}
+};
