2b01fd2d3cc6d8d8ec19cb97f9e6781990385c28
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-03-10 19:41:09 +0100

    s: tipi rigenerati (e file rinominati)

--- shared/src/types/typesFixes.ts
+++ shared/src/types/typesFixes.ts
@@ -1,10 +1,10 @@
-import { UsersPermissionsMe } from "./strapi";
+import { UsersPermissionsMe } from "./types";
 
 export type ID<T> = T & { id: string };
 
 export type Comp<T> = Partial<T> & { __component: string };
 
 export interface LoginResponse {
-    jwt: string;
-    user: UsersPermissionsMe;
+	jwt?: string;
+	user: UsersPermissionsMe;
 }
