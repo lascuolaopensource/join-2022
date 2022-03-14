25503952141e175e45c1532f2e1b635b69277d44
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-03-14 17:46:33 +0100

    be: added getUserByEmail fn

--- backend-strapi/src_ts/utils/index.ts
+++ backend-strapi/src_ts/utils/index.ts
@@ -1,13 +1,17 @@
 import { t } from "shared";
 
-export async function getCourseByID(
-    id: string | number,
-    options = {}
-): Promise<t.ID<t.Course>> {
+export async function getCourseByID(id: string | number, options = {}) {
     const course: t.ID<t.Course> = await strapi.entityService.findOne(
         "api::course.course",
         id,
         options
     );
     return course;
+}
+
+export async function getUserByEmail(email: string) {
+    const user: t.ID<t.UsersPermissionsUser> = await strapi
+        .query("plugin::users-permissions.user")
+        .findOne({ where: { email } });
+    return user;
 }
