bfcc6fd1a4ad980d9bcbb52ffd7f77143cc4d532
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-03-14 16:27:56 +0100

    be: utils small fix

--- backend-strapi/src/utils/index.js
+++ backend-strapi/src/utils/index.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getCourseByID = void 0;
-async function getCourseByID(id) {
-    const course = await strapi.entityService.findOne("api::course.course", id);
+async function getCourseByID(id, options = {}) {
+    const course = await strapi.entityService.findOne("api::course.course", id, options);
     return course;
 }
 exports.getCourseByID = getCourseByID;
--- backend-strapi/src_ts/utils/index.ts
+++ backend-strapi/src_ts/utils/index.ts
@@ -1,11 +1,13 @@
 import { t } from "shared";
 
 export async function getCourseByID(
-    id: string | number
+    id: string | number,
+    options = {}
 ): Promise<t.ID<t.Course>> {
     const course: t.ID<t.Course> = await strapi.entityService.findOne(
         "api::course.course",
-        id
+        id,
+        options
     );
     return course;
 }
