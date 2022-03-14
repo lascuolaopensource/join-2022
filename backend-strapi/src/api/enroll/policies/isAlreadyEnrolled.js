1761f882e6d94626c5d611654b70ec9df906066a
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-03-14 16:28:32 +0100

    be: created alreadyEnrolled policy

---
+++ backend-strapi/src/api/enroll/policies/isAlreadyEnrolled.js
@@ -0,0 +1,37 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const utils = require("@strapi/utils");
+const { PolicyError } = utils.errors;
+const utils_1 = require("../../../utils");
+module.exports = async (policyContext, config, { strapi }) => {
+    strapi.log.info("In isAlreadyEnrolled policy.");
+    const body = policyContext.request.body;
+    const course = await (0, utils_1.getCourseByID)(body.courseId, {
+        populate: {
+            enrollments: {
+                populate: {
+                    owner: {
+                        fields: ["id", "email"],
+                    },
+                },
+            },
+        },
+    });
+    const enrollments = course.enrollments;
+    let userEmail;
+    if (policyContext.state.user) {
+        userEmail = policyContext.state.user.email;
+    }
+    else {
+        userEmail = body.contacts.user.email.toLowerCase();
+    }
+    for (let e of enrollments) {
+        const owner = e.owner;
+        if (owner.email == userEmail) {
+            throw new PolicyError("alreadyEnrolled", {
+                policy: "isAlreadyEnrolled",
+            });
+        }
+    }
+    return true;
+};
