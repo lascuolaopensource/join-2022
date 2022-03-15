fed6cb03b8706be30a01560ee612402b7f5a2c82
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-03-15 15:58:21 +0100

    be: created api "is-user-enrolled"

--- backend-strapi/src/utils/index.js
+++ backend-strapi/src/utils/index.js
@@ -1,14 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUserByEmail = exports.getCourseByID = void 0;
+exports.getUserByEmail = exports.getCourseByID = exports.entities = void 0;
+exports.entities = {
+    course: "api::course.course",
+    user: "plugin::users-permissions.user",
+};
 async function getCourseByID(id, options = {}) {
-    const course = await strapi.entityService.findOne("api::course.course", id, options);
+    const course = await strapi.entityService.findOne(exports.entities.course, id, options);
     return course;
 }
 exports.getCourseByID = getCourseByID;
 async function getUserByEmail(email) {
     const user = await strapi
-        .query("plugin::users-permissions.user")
+        .query(exports.entities.user)
         .findOne({ where: { email } });
     return user;
 }
