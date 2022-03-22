1b547f7c590e82e417e61134cf727c71ab7d1192
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-03-22 18:52:47 +0100

    be: moved user-exists routes into src_ts, renamed path

---
+++ backend-strapi/src/api/user-exists/routes/user-exists.js
@@ -0,0 +1,10 @@
+"use strict";
+module.exports = {
+    routes: [
+        {
+            method: "POST",
+            path: "/user-exists",
+            handler: "user-exists.index",
+        },
+    ],
+};
---
+++ backend-strapi/src_ts/api/user-exists/routes/user-exists.ts
@@ -0,0 +1,9 @@
+module.exports = {
+    routes: [
+        {
+            method: "POST",
+            path: "/user-exists",
+            handler: "user-exists.index",
+        },
+    ],
+};
