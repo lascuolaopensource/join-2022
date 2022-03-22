7804334b0d1e9efd23fd4e17db2875753df4cc43
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-03-22 12:43:21 +0100

    fe: updated pay confirm page

--- frontend-sveltekit/src/routes/shared/[course]/enrollConfirm.svelte
+++ frontend-sveltekit/src/routes/shared/[course]/enrollConfirm.svelte
@@ -1,1 +1,12 @@
-Grazie per esserti iscritto! Bla bla bla
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { user } from '$lib/stores';
+
+	const courseTitle = decodeURIComponent($page.params.course);
+</script>
+
+Grazie per esserti iscritto a {courseTitle}!<br />
+Ti notificheremo sugli sviluppi del corso :)
+{#if $user}
+	<a href="/inside">Torna alla home</a>
+{/if}
