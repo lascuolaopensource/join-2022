9fec76feb01ac97ecfc99688745c4ad434f358a4
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-03-30 18:48:36 +0200

    be: created isBodyValid policy

---
+++ backend/src_ts/policies/isBodyValid.ts
@@ -0,0 +1,30 @@
+import * as yup from "yup";
+import { Errors as E, e } from "shared";
+
+const utils = require("@strapi/utils");
+const { PolicyError } = utils.errors;
+
+module.exports = async (policyContext: any, config: any, { strapi }: any) => {
+    const policy = "isBodyValid";
+    strapi.log.info(`In ${policy} policy`);
+
+    // Getting validator
+    const schema: yup.AnySchema = config.schema;
+
+    // Throwing error if no schema
+    if (!schema) {
+        throw new PolicyError(E.NoSchemaProvidedInPolicyConfig, { policy });
+    }
+
+    // Getting body
+    const body: e.PayReq = policyContext.request.body;
+
+    // Validating body
+    try {
+        await schema.validate(body);
+    } catch (e) {
+        throw new PolicyError(E.InvalidRequestBody, { policy });
+    }
+
+    return true;
+};
