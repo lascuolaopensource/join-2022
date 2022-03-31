95fe8cbc4f4ab5ee2314f2bf27bb4240e6e819ab
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-03-31 20:19:51 +0200

    fe: added active enrollments

--- frontend/src/routes/inside/profile/enrollments.svelte
+++ frontend/src/routes/inside/profile/enrollments.svelte
@@ -1,21 +1,41 @@
 <script lang="ts">
 	import { req } from '$lib/requestUtils';
 	import { Loading } from '$lib/components';
-	import { types as t } from 'shared';
+	import { types as t, formatters as f } from 'shared';
 
 	const promise = req.getUserEnrollments();
 </script>
+
+<!--  -->
 
 {#await promise}
 	<Loading />
 {:then res}
 	{@const enrollments = res.enrollments}
+
+	<h1>Iscrizioni attive</h1>
 	{@const enrollmentsPending = enrollments.filter((e) => {
 		return e.state == t.Enum_Enrollment_State.Pending;
 	})}
 	{#each enrollmentsPending as en}
 		{@const course = en.course}
-		{en.state}
-		{course.title}
+		<div class="enrollment">
+			<p><strong>{course.title}</strong></p>
+			<p>Iscritto il: {f.formatDateString(en.createdAt)}</p>
+			<p>Stato: {en.state}</p>
+		</div>
 	{/each}
 {/await}
+
+<!--  -->
+<style>
+	h1 {
+		margin-bottom: var(--s-3);
+	}
+
+	.enrollment {
+		background-color: white;
+		border: 1px solid gray;
+		padding: var(--s-2);
+	}
+</style>
