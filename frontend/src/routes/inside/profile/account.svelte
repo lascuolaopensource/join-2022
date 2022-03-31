7e95bd4595f4612e3cc1857a144ee07f3fb139bc
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-03-31 19:11:03 +0200

    fe: profile/index redirects to profile/account

---
+++ frontend/src/routes/inside/profile/account.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { user } from '$lib/stores';
+
+	let username = $user.username;
+	let email = $user.email;
+
+	let readonly = true;
+
+	// To edit profile:
+	// https://www.drewtown.dev/post/using-strapi-policies-to-create-editable-user-profiles/
+
+	// function edit() {
+	// 	readonly = false;
+	// }
+
+	// function save() {
+	// 	readonly = true;
+	// }
+
+	// function undo() {
+	// 	readonly = true;
+	// 	username = $user.username;
+	// 	email = $user.email;
+	// }
+</script>
+
+<!-- <input {readonly} type="text" bind:value={username} />
+<input {readonly} type="text" bind:value={email} /> -->
+
+Qui ci saranno le tue info personali
+
+<!-- {#if readonly}
+	<button on:click={edit}>Edit data</button>
+{:else}
+	<button on:click={save}>Save edits</button>
+	<button on:click={undo}>Undo</button>
+{/if} -->
