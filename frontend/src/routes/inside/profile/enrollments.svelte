b9c5b2ad3658908c58086bb2f1f9de5d94cbd9bb
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-04-05 12:31:36 +0200

    fe: updated user enrollments page

--- frontend/src/routes/inside/profile/enrollments.svelte
+++ frontend/src/routes/inside/profile/enrollments.svelte
@@ -1,9 +1,47 @@
 <script lang="ts">
 	import { req } from '$lib/requestUtils';
 	import { Loading } from '$lib/components';
-	import { types as t, formatters as f } from 'shared';
+	import { types as t, formatters as f, helpers as h } from 'shared';
+	import { enrollmentStates } from '$lib/strings';
 
-	const promise = req.getUserEnrollments();
+	const promise = (async () => {
+		const enrollmentsReq = await req.getUserEnrollments();
+		const enrollments = enrollmentsReq.enrollments;
+		const today = new Date().valueOf();
+
+		// Current enrollments
+		const enrollments_current = enrollments.filter((e) => {
+			const startDate = Date.parse(h.course.getStartDate(e.course));
+			return today < startDate && e.state != t.Enum_Enrollment_State.Rejected;
+		});
+		enrollments_current.sort((a, b) => {
+			return (
+				Date.parse(a.course.enrollmentDeadline) -
+				Date.parse(b.course.enrollmentDeadline)
+			);
+		});
+
+		// Past enrollments
+		const enrollments_past = enrollments.filter((e) => {
+			const endDate = Date.parse(h.course.getEndDate(e.course));
+			return today > endDate || e.state == t.Enum_Enrollment_State.Rejected;
+		});
+		enrollments_past.sort((a, b) => {
+			return (
+				Date.parse(a.course.enrollmentDeadline) -
+				Date.parse(b.course.enrollmentDeadline)
+			);
+		});
+
+		return { enrollments_current, enrollments_past };
+	})();
+
+	const statesColors: Record<t.Enum_Enrollment_State, string> = {
+		pending: 'orange',
+		awaitingPayment: 'orange',
+		approved: 'green',
+		rejected: 'red'
+	};
 </script>
 
 <!--  -->
@@ -11,20 +49,42 @@
 {#await promise}
 	<Loading />
 {:then res}
-	{@const enrollments = res.enrollments}
+	<!-- Iscrizioni attive -->
 
-	<h1>Iscrizioni attive</h1>
-	{@const enrollmentsPending = enrollments.filter((e) => {
-		return e.state == t.Enum_Enrollment_State.Pending;
-	})}
-	{#each enrollmentsPending as en}
-		{@const course = en.course}
-		<div class="enrollment">
-			<p><strong>{course.title}</strong></p>
-			<p>Iscritto il: {f.formatDateString(en.createdAt)}</p>
-			<p>Stato: {en.state}</p>
-		</div>
-	{/each}
+	{#if res.enrollments_current.length}
+		<h1>Iscrizioni attive</h1>
+
+		{#each res.enrollments_current as en}
+			{@const course = en.course}
+			<div class="enrollment">
+				<p><strong>{course.title}</strong></p>
+				<p>Iscritto il: {f.formatDateString(en.createdAt)}</p>
+				<p>
+					Stato: <span style:color={statesColors[en.state]}
+						>{enrollmentStates[en.state]}</span
+					>
+				</p>
+			</div>
+		{/each}
+	{/if}
+
+	<!-- Iscrizioni passate -->
+	{#if res.enrollments_past.length}
+		<h1>Iscrizioni passate</h1>
+
+		{#each res.enrollments_past as en}
+			{@const course = en.course}
+			<div class="enrollment">
+				<p><strong>{course.title}</strong></p>
+				<p>Iscritto il: {f.formatDateString(en.createdAt)}</p>
+				<p>
+					Stato: <span style:color={statesColors[en.state]}
+						>{enrollmentStates[en.state]}</span
+					>
+				</p>
+			</div>
+		{/each}
+	{/if}
 {/await}
 
 <!--  -->
