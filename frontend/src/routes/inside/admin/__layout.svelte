2ed0f841059b024815d4b3b9534521c434c956f0
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-04-07 08:49:28 +0200

    fe: added admin layout and index

---
+++ frontend/src/routes/inside/admin/__layout.svelte
@@ -0,0 +1,42 @@
+<!-- Here we check if the user is logged otherwise we send it away -->
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+
+	import { req } from '$lib/requestUtils';
+	import { userRole } from '$lib/stores';
+	import { types as t } from 'shared';
+
+	import { Loading } from '$lib/components';
+
+	//
+
+	// Si serve usare per "nascondere" lo slot fino a quando il caricamento non è completo
+	let loading = true;
+
+	// Quando il componente viene chiamato:
+	onMount(async () => {
+		try {
+			// Fetch the user's role
+			const userRoleRes = await req.getRole();
+			$userRole = userRoleRes.role;
+
+			// Redirect if not admin
+			if (userRoleRes.role != t.UserPermissionRoles.AdminEnrollments) {
+				await goto('/');
+			}
+
+			// Loading ends in that case
+			loading = false;
+		} catch (e) {
+			// Redirect if user is not found
+			await goto('/');
+		}
+	});
+</script>
+
+{#if loading}
+	<Loading />
+{:else}
+	<slot />
+{/if}
