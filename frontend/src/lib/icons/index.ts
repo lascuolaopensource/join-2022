93e646fc8e736ce6c8ce13ec6e1f822a5505fbe5
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-04-07 08:46:13 +0200

    fe: added icons

--- frontend/src/lib/icons/index.ts
+++ frontend/src/lib/icons/index.ts
@@ -5,6 +5,8 @@ import User24 from './User24.svelte';
 import WarningAlt24 from './WarningAlt24.svelte';
 import WarningAltFilled16 from './WarningAltFilled16.svelte';
 import WarningAltFilled24 from './WarningAltFilled24.svelte';
+import ArrowDown24 from './ArrowDown24.svelte';
+import ArrowUp24 from './ArrowUp24.svelte';
 
 export const icons = {
 	fields: {
@@ -19,5 +21,9 @@ export const icons = {
 		error: WarningAltFilled16,
 		close: Close24,
 		formError: WarningAlt24
+	},
+	arrows: {
+		up: ArrowUp24,
+		down: ArrowDown24
 	}
 };
