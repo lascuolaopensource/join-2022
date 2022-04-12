343ca30772096f5ba5c09372ee8fa2f2d2541b0b
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-04-12 16:05:31 +0200

    be: is-user-enrolled: added policies and error handling

--- backend/src/api/is-user-enrolled/controllers/is-user-enrolled.js
+++ backend/src/api/is-user-enrolled/controllers/is-user-enrolled.js
@@ -1,10 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const shared_1 = require("shared");
 const utils_1 = require("../../../utils");
 module.exports = {
     index: async (ctx, next) => {
         strapi.log.info("In Is-user-enrolled controller");
         const courseID = ctx.params.courseID;
+        if (!courseID) {
+            return ctx.badRequest(shared_1.Errors.MissingCourseID);
+        }
         const user = ctx.state.user;
         const userWithEnrollments = await strapi.entityService.findOne(utils_1.entities.user, user.id, {
             populate: {
@@ -18,10 +22,12 @@ module.exports = {
             },
         });
         const enrollments = userWithEnrollments.enrollments;
-        for (let e of enrollments) {
-            const course = e.course;
-            if (course.id == courseID) {
-                return { enrolled: true };
+        if (enrollments.length) {
+            for (let e of enrollments) {
+                const course = e.course;
+                if (course.id == courseID) {
+                    return { enrolled: true };
+                }
             }
         }
         return { enrolled: false };
