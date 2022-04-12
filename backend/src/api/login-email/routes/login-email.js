60e72542574f6ea9e0a51a97b25da0796d98aa76
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-04-12 15:42:29 +0200

    be: login-email: added policy, added error

--- backend/src/api/login-email/routes/login-email.js
+++ backend/src/api/login-email/routes/login-email.js
@@ -1,10 +1,16 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const utils_1 = require("../../../utils");
+const shared_1 = require("shared");
 module.exports = {
     routes: [
         {
             method: "POST",
             path: "/login-email",
             handler: "login-email.index",
+            config: {
+                policies: [(0, utils_1.policyIsBodyValid)(shared_1.endpoints.LoginEmailSchema)],
+            },
         },
     ],
 };
--- backend/src_ts/api/login-email/routes/login-email.ts
+++ backend/src_ts/api/login-email/routes/login-email.ts
@@ -1,9 +1,15 @@
+import { policyIsBodyValid } from "../../../utils";
+import { endpoints as e } from "shared";
+
 module.exports = {
     routes: [
         {
             method: "POST",
             path: "/login-email",
             handler: "login-email.index",
+            config: {
+                policies: [policyIsBodyValid(e.LoginEmailSchema)],
+            },
         },
     ],
 };
