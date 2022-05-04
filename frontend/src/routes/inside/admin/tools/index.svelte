1f7b8520a136949103ea028368cb2629f7bc91ff
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-05-04 19:45:07 +0200

    fe: some tests for tools admin

--- frontend/src/routes/inside/admin/tools/index.svelte
+++ frontend/src/routes/inside/admin/tools/index.svelte
@@ -1,42 +1,59 @@
 <script lang="ts">
 	import { tools } from '$lib/stores';
 	import { MiniCal, MiniCalCell } from '$lib/components';
+	import { miniCalCellStates } from '$lib/components/miniCalCell.svelte';
 	import type {
 		MiniCalCellState,
 		EventDetail
 	} from '$lib/components/miniCalCell.svelte';
+	import _ from 'lodash';
 
-	const start = new Date();
+	//
+
 	const days = 7;
+	const hours = 24;
 
-	const cal = (() => {
-		const cal = {};
+	let weekStart = getMonday();
+
+	function getMonday() {
+		const d = new Date();
+		d.setHours(0, 0, 0, 0);
+
+		const day = d.getDay();
+		const diff = d.getDate() - day + (day == 0 ? -6 : 1); // adjust when day is sunday
+		return new Date(d.setDate(diff));
+	}
+
+	const cals = {};
+
+	$tools.forEach((t) => {
+		cals[t.id] = {};
 
 		// Iterating over days
 		for (let d = 0; d < days; d++) {
 			// Creating the date
-			const date = new Date(start.getTime());
-			date.setDate(start.getDate() + d);
+			const date = new Date(weekStart.getTime());
+			date.setDate(weekStart.getDate() + d);
 
 			// Randomly generating states
-			for (let i = 0; i < 4; i++) {
-				const randomHour = Math.round(Math.random() * 24);
-				date.setHours(randomHour, 0, 0, 0);
-				cal[date.toISOString()] = Math.random() > 0.5 ? 'booked' : 'blocked';
+			for (let i = 0; i < hours; i++) {
+				date.setHours(i, 0, 0, 0);
+				cals[t.id][date.toISOString()] = _.sample(miniCalCellStates);
 			}
 		}
-		return cal;
-	})();
+	});
 
-	function getState(datetime: Date): MiniCalCellState {
-		const state = cal[datetime.toISOString()];
-		return state ? state : 'free';
-	}
+	//
 
 	let edits: Record<string, Record<string, MiniCalCellState>> = {};
-	$tools.forEach((t) => {
-		edits[t.id] = {};
-	});
+	createEmptyEdits();
+
+	function createEmptyEdits() {
+		edits = {};
+		$tools.forEach((t) => {
+			edits[t.id] = {};
+		});
+	}
 
 	function updateEdits(e: CustomEvent<EventDetail>, toolID: string) {
 		//
@@ -52,32 +69,78 @@
 		// Reassigning object to trigger change
 		edits[toolID] = { ...edits[toolID] };
 	}
+
+	function editsExist() {
+		for (const toolEdits of Object.values(edits)) {
+			if (Object.values(toolEdits).length > 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function changeWeek(sign: -1 | 1) {
+		createEmptyEdits();
+		// if (!editsExist()) {
+		weekStart.setDate(weekStart.getDate() + 7 * sign);
+		weekStart = weekStart;
+		// }
+	}
 </script>
 
 <!--  -->
 
-<div class="main__cont">
-	{#each $tools as t}
-		<div class="cal__container">
-			<p><strong>{t.attributes.name}</strong></p>
-			<MiniCal {start} let:dateTime>
-				{@const state = getState(dateTime)}
-				<MiniCalCell
-					{dateTime}
-					{state}
-					on:edit={(e) => {
-						updateEdits(e, t.id);
-					}}
-				/>
-			</MiniCal>
+<main>
+	<div class="sidebar">
+		<button
+			on:click={() => {
+				changeWeek(-1);
+			}}>-</button
+		>
+		<button
+			on:click={() => {
+				changeWeek(1);
+			}}>+</button
+		>
+		{weekStart}
+	</div>
+
+	<div class="cals__container">
+		{#each $tools as t}
+			<div class="cal__container">
+				<p><strong>{t.attributes.name}</strong></p>
+				<MiniCal start={weekStart} {hours} let:dateTime>
+					<MiniCalCell
+						{dateTime}
+						bind:state={cals[t.id][dateTime.toISOString()]}
+						on:edit={(e) => {
+							updateEdits(e, t.id);
+						}}
+					/>
+				</MiniCal>
+			</div>
 			<pre>{JSON.stringify(edits[t.id], null, 2)}</pre>
-		</div>
-	{/each}
-</div>
+		{/each}
+	</div>
+</main>
 
 <!--  -->
 <style>
-	.main__cont {
+	button {
+		width: 40px;
+		height: 40px;
+	}
+
+	main {
+		display: flex;
+		flex-flow: row nowrap;
+	}
+
+	.sidebar {
+		flex-basis: 40%;
+	}
+
+	.cals__container {
 		display: flex;
 		flex-flow: row wrap;
 		gap: var(--s-2);
