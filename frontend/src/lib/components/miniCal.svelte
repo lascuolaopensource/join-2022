d1660ae3ed04db3bc9c0dc4cc27806ecd14389a8
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-05-04 17:34:58 +0200

    fe: update minical with slot props

--- frontend/src/lib/components/miniCal.svelte
+++ frontend/src/lib/components/miniCal.svelte
@@ -1,47 +1,71 @@
 <script lang="ts">
-	export let start: Date;
+	export let start = new Date();
 	export let hours = 24;
 	export let days = 7;
 
-	const total = hours * days;
+	//
 
+	// Setting the start date time to zero
+	start.setHours(0, 0, 0, 0);
+
+	// Listing the days
 	const daysList: Array<Date> = [];
 	for (let i = 0; i < days; i++) {
 		// Create new date instance
-		const newDate = new Date();
+		const newDate = new Date(start.getTime());
 		// Add a day
 		newDate.setDate(start.getDate() + i);
 		//
 		daysList.push(newDate);
 	}
 
-	const hoursList: Array<string> = [];
+	// Listing the hours
+	const hoursList: Array<number> = [];
 	for (let i = 0; i < hours; i++) {
-		hoursList.push(
-			i.toLocaleString('it-IT', {
-				minimumIntegerDigits: 2,
-				useGrouping: false
-			})
-		);
+		hoursList.push(i);
+	}
+
+	//
+
+	// Hour formatter
+	function formatHour(h: number) {
+		return h.toLocaleString('it-IT', {
+			minimumIntegerDigits: 2,
+			useGrouping: false
+		});
+	}
+
+	// Datetime builder
+	function buildDateTime(d: Date, h: number) {
+		const date = new Date(d.getTime());
+		date.setHours(h);
+		return date;
 	}
 </script>
 
 <!--  -->
 
-{start}
 <div class="miniCal" style:--cols={days + 1} style:--rows={hours + 1}>
-	<!-- Header -->
+	<!-- First empty cell -->
 	<div class="miniCal__header" />
+
+	<!-- Hours column -->
+	{#each hoursList as h}
+		<div class="miniCal__hour">{formatHour(h)}</div>
+	{/each}
+
+	<!-- Days columns -->
 	{#each daysList as d}
+		<!-- Day header -->
 		<div class="miniCal__header">
 			{d.getDate()}
 		</div>
-	{/each}
-	<!-- Rows -->
-	{#each hoursList as h}
-		<div class="miniCal__hour">{h}</div>
-		{#each { length: days } as _, d}
-			<div class="miniCal__cell">yolo</div>
+		<!-- Hours per day -->
+		{#each hoursList as h}
+			{@const dateTime = buildDateTime(d, h)}
+			<div class="miniCal__cell">
+				<slot {dateTime} />
+			</div>
 		{/each}
 	{/each}
 </div>
@@ -50,6 +74,7 @@
 <style>
 	.miniCal {
 		display: grid;
+		grid-auto-flow: column;
 		grid-template-rows: repeat(var(--rows), 1fr);
 		grid-template-columns: repeat(var(--cols), 1fr);
 	}
