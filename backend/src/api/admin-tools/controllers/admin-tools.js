909931ff90a1a7dd65b0084d5c6f9027a740e408
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-05-06 20:58:37 +0200

    be: setup base admin-tools endpoint

--- backend/src/api/admin-tools/controllers/admin-tools.js
+++ backend/src/api/admin-tools/controllers/admin-tools.js
@@ -1,7 +1,11 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 module.exports = {
     updateSlots: async (ctx, next) => {
         strapi.log.info("In admin-tools/updateSlots controller");
+        const { changes } = ctx.request.body;
+        changes.forEach(async (c) => {
+        });
         return {};
     },
 };
--- backend/src_ts/api/admin-tools/controllers/admin-tools.ts
+++ backend/src_ts/api/admin-tools/controllers/admin-tools.ts
@@ -1,10 +1,26 @@
-module.exports = {
-    /**
-     *
-     */
+import { endpoints as e, types as t, helpers as h } from "shared";
+import { entities } from "../../../utils";
 
+module.exports = {
     updateSlots: async (ctx: any, next: any) => {
         strapi.log.info("In admin-tools/updateSlots controller");
+
+        // Getting body
+        const { changes } = ctx.request.body as e.AdminToolsUpdateSlotsReq;
+
+        changes.forEach(async (c) => {
+            // const date = h.date.formatQueryDate(
+            //     new Date(Date.parse(c.dateTime))
+            // );
+            // const time =
+            // console.log(date);
+            // const slot: t.ID<t.ToolSlot> = await strapi.entityService.findMany(
+            //     entities.toolSlot,
+            //     {}
+            // );
+            // Controllare se esiste slot con tale configurazione
+            // Applicare cambiamento
+        });
 
         return {};
     },
