6598ac18e94fe78b3134ccc89a23d37471ff560e
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-05-15 21:08:58 +0200

    be: admin-tools update

--- backend/src/api/admin-tools/controllers/admin-tools.js
+++ backend/src/api/admin-tools/controllers/admin-tools.js
@@ -1,11 +1,46 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const shared_1 = require("shared");
+const utils_1 = require("../../../utils");
 module.exports = {
     updateSlots: async (ctx, next) => {
         strapi.log.info("In admin-tools/updateSlots controller");
         const { changes } = ctx.request.body;
-        console.log(changes);
         changes.forEach(async (c) => {
+            const date = new Date(Date.parse(c.dateTime));
+            const dateStr = shared_1.helpers.date.formatQueryDate(date);
+            const time_start = shared_1.helpers.date.getTimeString(date);
+            date.setHours(date.getHours() + 1);
+            const time_end = shared_1.helpers.date.getTimeString(date);
+            if (c.state === null) {
+                const slots = await strapi.entityService.findMany(utils_1.entities.toolSlot, {
+                    filters: {
+                        date: dateStr,
+                        time_start,
+                        time_end,
+                    },
+                });
+                slots.forEach(async (s) => {
+                    await strapi.entityService.delete(utils_1.entities.toolSlot, s.id);
+                });
+            }
+            else if ((c.state = shared_1.types.Enum_Toolslot_Type.Block)) {
+                const data = {
+                    date: dateStr,
+                    time_start,
+                    time_end,
+                    type: shared_1.types.Enum_Toolslot_Type.Block,
+                    tool: c.toolID,
+                };
+                try {
+                    const slot = await strapi.entityService.create(utils_1.entities.toolSlot, {
+                        data,
+                    });
+                }
+                catch (e) {
+                    console.log(e);
+                }
+            }
         });
         return {};
     },
--- backend/src_ts/api/admin-tools/controllers/admin-tools.ts
+++ backend/src_ts/api/admin-tools/controllers/admin-tools.ts
@@ -7,20 +7,49 @@ module.exports = {
 
         // Getting body
         const { changes } = ctx.request.body as e.AdminToolsUpdateSlotsReq;
-        console.log(changes);
 
         changes.forEach(async (c) => {
-            // const date = h.date.formatQueryDate(
-            //     new Date(Date.parse(c.dateTime))
-            // );
-            // const time =
-            // console.log(date);
-            // const slot: t.ID<t.ToolSlot> = await strapi.entityService.findMany(
-            //     entities.toolSlot,
-            //     {}
-            // );
-            // Controllare se esiste slot con tale configurazione
-            // Applicare cambiamento
+            // Getting time data
+            const date: Date = new Date(Date.parse(c.dateTime));
+            const dateStr: string = h.date.formatQueryDate(date);
+            const time_start = h.date.getTimeString(date);
+            date.setHours(date.getHours() + 1);
+            const time_end = h.date.getTimeString(date);
+
+            // Removing slot if state === null
+            if (c.state === null) {
+                // Finding matching slot
+                const slots: Array<t.ID<t.ToolSlot>> =
+                    await strapi.entityService.findMany(entities.toolSlot, {
+                        filters: {
+                            date: dateStr,
+                            time_start,
+                            time_end,
+                        },
+                    });
+                slots.forEach(async (s) => {
+                    await strapi.entityService.delete(entities.toolSlot, s.id);
+                });
+            }
+            // Otherwise creating slot
+            else if ((c.state = t.Enum_Toolslot_Type.Block)) {
+                // Creating slot
+                const data: t.ToolSlotInput = {
+                    date: dateStr,
+                    time_start,
+                    time_end,
+                    type: t.Enum_Toolslot_Type.Block,
+                    tool: c.toolID,
+                };
+                try {
+                    const slot: t.ID<t.ToolSlot> =
+                        await strapi.entityService.create(entities.toolSlot, {
+                            data,
+                        });
+                } catch (e) {
+                    console.log(e);
+                }
+            }
         });
 
         return {};
