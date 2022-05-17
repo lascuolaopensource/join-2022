948681adbd0be20fc1354d44d90842a04dfe50f8
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-05-17 19:37:09 +0200

    be: started book-tools controller

--- backend/src/api/book-tools/controllers/book-tools.js
+++ backend/src/api/book-tools/controllers/book-tools.js
@@ -1,15 +1,52 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const shared_1 = require("shared");
+const utils_1 = require("../../../utils");
+const lodash_1 = __importDefault(require("lodash"));
 module.exports = {
     book: async (ctx, next) => {
         strapi.log.info("In book-tools/index controller");
+        const body = ctx.request.body;
+        console.log(body);
     },
     checkAvailability: async (ctx, next) => {
         strapi.log.info("In book-tools/check-availability controller");
         const body = ctx.request.body;
-        console.log(body);
+        let tool_ids = body.days.reduce((prev, curr) => {
+            return [...prev, ...curr.tool_ids];
+        }, []);
+        tool_ids = lodash_1.default.uniq(tool_ids);
+        const date_start = new Date();
+        const date_end = shared_1.helpers.date.addDays(date_start, 7);
+        const slots = await strapi.entityService.findMany(utils_1.entities.toolSlot, {
+            populate: ["tool"],
+            filters: {
+                $and: [
+                    {
+                        tool: {
+                            id: {
+                                $in: tool_ids,
+                            },
+                        },
+                    },
+                    {
+                        date: {
+                            $gt: shared_1.helpers.date.formatQueryDate(date_start),
+                        },
+                    },
+                    {
+                        date: {
+                            $lt: shared_1.helpers.date.formatQueryDate(date_end),
+                        },
+                    },
+                ],
+            },
+        });
         try {
-            ctx.body = { ok: "ok" };
+            ctx.body = { tool_ids, slots };
         }
         catch (err) {
             ctx.body = { err };
--- backend/src_ts/api/book-tools/controllers/book-tools.ts
+++ backend/src_ts/api/book-tools/controllers/book-tools.ts
@@ -1,7 +1,8 @@
 "use strict";
 
-import { endpoints as e, types as t } from "shared";
+import { endpoints as e, types as t, helpers as h } from "shared";
 import { entities } from "../../../utils";
+import _ from "lodash";
 
 /**
  * A set of functions called "actions" for `book-tools`
@@ -10,6 +11,9 @@ import { entities } from "../../../utils";
 module.exports = {
     book: async (ctx: any, next: any) => {
         strapi.log.info("In book-tools/index controller");
+
+        const body = ctx.request.body;
+        console.log(body);
 
         // // Getting data
         // const body: e.BookToolsBookReq = ctx.request.body;
@@ -61,10 +65,51 @@ module.exports = {
         strapi.log.info("In book-tools/check-availability controller");
 
         const body: e.BookToolsCheckAvailabilityReq = ctx.request.body;
-        console.log(body);
+
+        /**
+         * Getting all the required tools
+         */
+
+        let tool_ids = body.days.reduce((prev, curr) => {
+            return [...prev, ...curr.tool_ids];
+        }, [] as Array<string>);
+        tool_ids = _.uniq(tool_ids);
+
+        /**
+         * Getting free slots for each tool
+         */
+
+        const date_start = new Date();
+        const date_end = h.date.addDays(date_start, 7);
+
+        const slots: Array<t.ID<t.ToolSlot>> =
+            await strapi.entityService.findMany(entities.toolSlot, {
+                populate: ["tool"],
+                filters: {
+                    $and: [
+                        {
+                            tool: {
+                                id: {
+                                    $in: tool_ids,
+                                },
+                            },
+                        },
+                        {
+                            date: {
+                                $gt: h.date.formatQueryDate(date_start),
+                            },
+                        },
+                        {
+                            date: {
+                                $lt: h.date.formatQueryDate(date_end),
+                            },
+                        },
+                    ],
+                },
+            });
 
         try {
-            ctx.body = { ok: "ok" };
+            ctx.body = { tool_ids, slots };
         } catch (err) {
             ctx.body = { err };
         }
