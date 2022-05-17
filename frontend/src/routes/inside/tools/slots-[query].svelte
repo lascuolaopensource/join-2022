8ae15bdda771c3ca1d05e46935ead4b69ac5d74e
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-05-17 19:39:18 +0200

    fe: started tools query results

--- frontend/src/routes/inside/tools/slots-[query].svelte
+++ frontend/src/routes/inside/tools/slots-[query].svelte
@@ -3,6 +3,12 @@
 	import { req } from '$lib/requestUtils';
 
 	const query = JSON.parse(decodeURIComponent($page.params.query));
+	const promise = req.checkSlots({ days: query });
 </script>
 
+<!--  -->
+
 <pre>{JSON.stringify(query, null, 2)}</pre>
+{#await promise then res}
+	<pre>{JSON.stringify(res, null, 2)}</pre>
+{/await}
