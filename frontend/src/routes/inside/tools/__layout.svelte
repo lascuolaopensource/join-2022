c396c18ad6e0395817c1bb4cdc74243fb960423c
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-06-09 11:47:00 +0200

    fe: tool: updates
    - needs è stata riscritta utilizzando il nuovo componente "DayCard"
    - needs salva il contenuto della richiesta in uno store, anzichè mandarlo come url
    - slots-[query] è stato rinominato in availabilities, visto che adesso le informazioni sono passate tramite store, anzichè url
    - in layout e in needs i rimandi a availabilities sono stati sistemati
    - piccola modifica di stile a __layout

--- frontend/src/routes/inside/tools/__layout.svelte
+++ frontend/src/routes/inside/tools/__layout.svelte
@@ -2,13 +2,13 @@
 	import { req } from '$lib/requestUtils';
 	import { tools } from '$lib/stores';
 
-	import { Container, Steps } from '$lib/components';
+	import { Steps } from '$lib/components';
 	import type { StepData } from '$lib/components/step.svelte';
 
 	const data: Array<StepData> = [
 		{ href: '/inside/tools', label: 'Start' },
 		{ href: '/inside/tools/needs', label: 'Cosa' },
-		{ href: '/inside/tools/slots-', label: 'Quando' }
+		{ href: '/inside/tools/availabilities', label: 'Quando' }
 	];
 
 	// Fetching tools
@@ -23,16 +23,8 @@
 {#await promise}
 	loading
 {:then res}
-	<Container>
+	<div class="container mx-auto p-6">
 		<Steps {data} />
-		<div class="spacer" />
-		<slot />
-	</Container>
-
-	<style>
-		.spacer {
-			width: 100%;
-			height: var(--s-3);
-		}
-	</style>
+	</div>
+	<slot />
 {/await}
