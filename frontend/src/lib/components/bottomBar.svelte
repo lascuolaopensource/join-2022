dd5e566aee701f567172a9257b3d3ec2a737579d
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-06-09 11:39:48 +0200

    fe: components: modifica
    - "modal" adesso richiede uno store (con il vecchio sistema due modali nella stessa pagina non funzionavano)
    - "button", "iconbutton" e "bottombar" css fix

--- frontend/src/lib/components/bottomBar.svelte
+++ frontend/src/lib/components/bottomBar.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	export let spaceBetween = false;
+	export let background: 'alert' | 'default' = 'alert';
 </script>
 
 <div
-	class="bg-yellow-100 fixed bottom-0 w-full left-0 py-4 px-6 flex flex-row items-center"
-	class:justify-between={spaceBetween}
+	class="sticky bottom-0 w-full left-0 py-4 px-6"
+	class:bg-yellow-100={background == 'alert'}
+	class:bg-white={background == 'default'}
 >
 	<slot />
 </div>
