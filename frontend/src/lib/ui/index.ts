028a64515eeff4924b5049ad5417f95ba2950cfa
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-06-29 17:41:59 +0200

    fe: added link component

--- frontend/src/lib/ui/index.ts
+++ frontend/src/lib/ui/index.ts
@@ -2,5 +2,12 @@ import CardWithButton from './cardWithButton.svelte';
 import SelectableTileControls from './selectableTileControls.svelte';
 import SelectableTile from './selectableTile.svelte';
 import WeekCal from './weekCal.svelte';
+import Link from './link.svelte';
 
-export { CardWithButton, SelectableTile, SelectableTileControls, WeekCal };
+export {
+	CardWithButton,
+	SelectableTile,
+	SelectableTileControls,
+	WeekCal,
+	Link
+};
---
+++ frontend/src/lib/ui/link.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	export let href: string;
+</script>
+
+<a class="text-blue-700 hover:text-white hover:bg-blue-700" {href}><slot /></a>
