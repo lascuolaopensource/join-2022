55b1979a087dc98e750919c2a3f1afa6759ebeb0
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-06-29 17:44:05 +0200

    fe: updated iconbutton

--- frontend/src/lib/components/iconButton.svelte
+++ frontend/src/lib/components/iconButton.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
 	export let icon: Function;
+	export let mode: 'light' | 'dark' = 'light';
 </script>
 
 <!--  -->
 
 <button
 	on:click
-	class="w-8 h-8 p-1 leading-none rounded-none hover:cursor-pointer bg-gray-300 hover:bg-gray-400"
+	class="
+		w-8 h-8 p-1 leading-none rounded-none hover:cursor-pointer
+		{mode == 'light' ? 'bg-gray-300 hover:bg-gray-400' : ''}
+		{mode == 'dark' ? 'bg-gray-700 hover:bg-gray-600' : ''}
+	"
 >
-	<svelte:component this={icon} class={'iconPrimary-l'} />
+	<svelte:component this={icon} class="fill-white" />
 </button>
