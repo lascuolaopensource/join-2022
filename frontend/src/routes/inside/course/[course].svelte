7c6f4c3656b45ba1e26dc1bd53054f2fb8e31b13
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-06-30 16:13:10 +0200

    fe: update course style

--- frontend/src/routes/inside/course/[course].svelte
+++ frontend/src/routes/inside/course/[course].svelte
@@ -2,7 +2,7 @@
 	import { req } from '$lib/requestUtils';
 
 	/** @type {import('@sveltejs/kit').Load} */
-	export async function load({ params, fetch, session, stuff }) {
+	export async function load({ params, fetch, session, stuff }: any) {
 		const slug = params.course;
 		const course = await req.getCourseBySlug(slug, fetch);
 
@@ -18,42 +18,55 @@
 <script lang="ts">
 	import { helpers as h, types as t } from 'shared';
 	import SvelteMarkdown from 'svelte-markdown';
+	import { goto } from '$app/navigation';
+
+	import { BottomBar, Button } from '$lib/components';
 
 	export let course: t.CourseEntity;
 	export let slug: string;
 
 	// Checking if user is enrolled
-	const promise = req.isUserEnrolled(course.id);
+	const promise = req.isUserEnrolled(course.id as string);
 
 	const c = course.attributes;
+
+	function goToEnroll() {
+		goto(`src/routes/shared/enroll/${slug}.svelte`);
+	}
 </script>
 
 <!--  -->
 
-<!-- Core corso -->
-<div class="cover">
-	<h1>{c.title}</h1>
-</div>
-{#if c.description}
-	<div class="markdown-body">
-		<SvelteMarkdown source={c.description} />
+{#if c}
+	<!-- Cover -->
+	<div class="cover">
+		<h1>{c.title}</h1>
 	</div>
-{/if}
 
-<!-- Iscriviti -->
-{#if h.course.isErollmentTime(c)}
-	<div class="iscriviti-container">
-		{#await promise then isUserEnrolled}
-			{#if isUserEnrolled.enrolled}
-				<div class="btn btn-shadow" disabled>Sei già iscritto!</div>
-			{:else}
-				<a
-					class="btn btn-primary btn-big btn-shadow"
-					href="/shared/enroll/{slug}">Iscriviti →</a
-				>
-			{/if}
-		{/await}
-	</div>
+	<!-- Descrizione -->
+	{#if c.description}
+		<div class="mx-auto prose p-4">
+			<SvelteMarkdown source={c.description} />
+		</div>
+	{/if}
+
+	<!-- Iscriviti -->
+	{#if h.course.isErollmentTime(c)}
+		<BottomBar background="blur">
+			<div class="flex justify-end">
+				{#await promise then isUserEnrolled}
+					{@const isEnrolled = isUserEnrolled.enrolled}
+					<Button on:click={goToEnroll} disabled={isEnrolled}>
+						{#if isEnrolled}
+							Sei già iscritto!
+						{:else}
+							Iscriviti →
+						{/if}
+					</Button>
+				{/await}
+			</div>
+		</BottomBar>
+	{/if}
 {/if}
 
 <!--  -->
@@ -72,20 +85,5 @@
 
 	.cover h1 {
 		text-align: center;
-	}
-
-	.markdown-body {
-		padding: 20px;
-	}
-
-	.iscriviti-container {
-		position: sticky;
-		bottom: 0;
-		width: 100%;
-		padding: 20px;
-		display: flex;
-		flex-flow: row nowrap;
-		align-items: center;
-		justify-content: flex-end;
 	}
 </style>
