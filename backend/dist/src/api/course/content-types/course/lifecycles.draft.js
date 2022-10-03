bc7cbb3403c73e72d3c211ce6af6e008d1f20288
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-10-03 11:55:46 +0200

    dumped config

--- backend/dist/src/api/course/content-types/course/lifecycles.draft.js
+++ backend/dist/src/api/course/content-types/course/lifecycles.draft.js
@@ -1,11 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getMeetings() { }
-exports.default = {
-    beforeUpdate(event) {
-        const { data } = event.params;
-        const deadline = new Date(data.enrollmentDeadline);
-        const meetings = data.meetings;
-        console.log(event.params.where.id, deadline, meetings);
-    },
-};
+// import { LifecycleEvent } from "@utils";
+// import { types as t } from "join-shared";
+// function getMeetings() {}
+// export default {
+//     beforeUpdate(event: LifecycleEvent<t.Course, any>) {
+//         const { data } = event.params;
+//         const deadline = new Date(data.enrollmentDeadline);
+//         const meetings = data.meetings;
+//         console.log(, deadline, meetings);
+//     },
+// };
--- backend/src/api/course/content-types/course/lifecycles.draft.ts
+++ backend/src/api/course/content-types/course/lifecycles.draft.ts
@@ -1,14 +1,14 @@
-import { LifecycleEvent } from "@utils";
-import { types as t } from "join-shared";
+// import { LifecycleEvent } from "@utils";
+// import { types as t } from "join-shared";
 
-function getMeetings() {}
+// function getMeetings() {}
 
-export default {
-    beforeUpdate(event: LifecycleEvent<t.Course, any>) {
-        const { data } = event.params;
+// export default {
+//     beforeUpdate(event: LifecycleEvent<t.Course, any>) {
+//         const { data } = event.params;
 
-        const deadline = new Date(data.enrollmentDeadline);
-        const meetings = data.meetings;
-        console.log(, deadline, meetings);
-    },
-};
+//         const deadline = new Date(data.enrollmentDeadline);
+//         const meetings = data.meetings;
+//         console.log(, deadline, meetings);
+//     },
+// };
