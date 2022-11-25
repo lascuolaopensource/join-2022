9fd2298f49e228d02f8543dd2c8b05cba05d5919
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-11-25 16:09:46 +0100

    created partials folder

---
+++ packages/apps/frontend/src/lib/partials/CourseCard.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { types as t } from 'join-shared';
+	import { formatters as f } from 'join-shared';
+	import { Card, Heading, P } from 'flowbite-svelte';
+
+	export let course: t.CourseEntity;
+	const c = course.attributes;
+</script>
+
+<Card href={`courses/${c?.slug}`}>
+	<div class="space-y-1">
+		<Heading tag="h5">{c?.name}</Heading>
+		<P>
+			<span class="font-semibold mr-1">Enrollment deadline:</span>
+			<span>
+				{f.formatDate(c?.enrollmentDeadline)}
+			</span>
+		</P>
+	</div>
+</Card>
