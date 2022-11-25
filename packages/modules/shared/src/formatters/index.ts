05157ccd9502edcb6cd33c5f570bfdad36161075
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-11-25 16:08:07 +0100

    fix(formatters): improved formatDate

--- packages/modules/shared/src/formatters/index.ts
+++ packages/modules/shared/src/formatters/index.ts
@@ -1,19 +1,23 @@
 export const formatPriceNumber = (
-	price: number,
-	locale = "IT-it",
-	currency = "EUR"
+    price: number,
+    locale = "IT-it",
+    currency = "EUR"
 ) => {
-	const formatter = new Intl.NumberFormat(locale, {
-		style: "currency",
-		currency,
-	});
-	return formatter.format(price);
+    const formatter = new Intl.NumberFormat(locale, {
+        style: "currency",
+        currency,
+    });
+    return formatter.format(price);
 };
 
-export function formatDate(date: Date, locale = "IT-it") {
-	return date.toLocaleDateString(locale, {
-		month: "2-digit",
-		day: "2-digit",
-		year: "2-digit",
-	});
+export function formatDate(date: Date | string, locale = "IT-it") {
+    let d: Date;
+    if (typeof date === "string") {
+        d = new Date(date);
+    }
+    return d.toLocaleDateString(locale, {
+        month: "2-digit",
+        day: "2-digit",
+        year: "2-digit",
+    });
 }
