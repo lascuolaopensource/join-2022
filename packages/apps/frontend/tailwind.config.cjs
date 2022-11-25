ba46ef50802dd9b69e89e5e57b0bc87a6a49c85a
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-11-25 19:17:27 +0100

    extended tailwind config

--- packages/apps/frontend/tailwind.config.cjs
+++ packages/apps/frontend/tailwind.config.cjs
@@ -1,8 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
 		'../../../node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
 	],
 	plugins: [require('flowbite/plugin'), require('@tailwindcss/typography')],
-	darkMode: 'class'
+	darkMode: 'class',
+	theme: {
+		extend: {
+			borderWidth: {
+				1: '1px'
+			}
+		}
+	}
 };
