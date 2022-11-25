920decb8a4f891f2546f9e7b96a094ac68e0e3d3
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-11-25 14:26:50 +0100

    feat(components) added title and text

---
+++ packages/apps/frontend/src/lib/components/TitleAndText.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Heading, P } from 'flowbite-svelte';
+
+	export let title: string | null = null;
+	export let text: string | null = null;
+	export let reverse: boolean = false;
+</script>
+
+{#if title || text}
+	<div class="flex" class:flex-col={!reverse} class:flex-col-reverse={reverse}>
+		{#if title}
+			<Heading tag="h3">{title}</Heading>
+		{/if}
+		{#if title && text}
+			<div class="h-1" />
+		{/if}
+		{#if text}
+			<P>{text}</P>
+		{/if}
+	</div>
+{/if}
