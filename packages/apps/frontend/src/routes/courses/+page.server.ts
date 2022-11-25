e8e3459469972a661b84f37de9e8921eb866e4f7
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-11-25 16:10:00 +0100

    created base courses page

--- packages/apps/frontend/src/routes/courses/+page.server.ts
+++ packages/apps/frontend/src/routes/courses/+page.server.ts
@@ -1,5 +1,61 @@
 import type { PageServerLoad } from './$types';
+import { getJWT } from '$lib/utils/cookies';
+import qs from 'qs';
+import { jr, Request, types as t } from 'join-shared';
 
-export const load: PageServerLoad = async () => {
-	return {};
+//
+
+export const load: PageServerLoad = async ({
+	cookies,
+	fetch
+}): Promise<t.CourseEntityResponseCollection | { error: string | undefined } | undefined> => {
+	const jwt = getJWT(cookies);
+
+	// This shouldn't happen, cause the user should be redirected to the login page before this
+	// But it's good to have this check here, for type safety
+	if (!jwt) {
+		console.log('no jwt');
+	}
+
+	// Building query
+	const query = qs.stringify({
+		populate: {
+			meetings: '*',
+			gallery: '*'
+		},
+		filters: {}
+	});
+
+	const res = await jr.send<t.CourseEntityResponseCollection>({
+		fetchImpl: fetch,
+		path: `/courses?${query}`,
+		auth: jwt,
+		method: Request.HTTPMethod.GET
+	});
+
+	if (!res.ok || !res.data) {
+		return { error: res.error?.error.message };
+	}
+
+	if (res.data) {
+		return res.data;
+	}
+
+	// // Getting courses
+	// const today = new Date().toISOString();
+	// // Deciding filter based on mode
+	// // lt: lower than | gt: greater than
+	// const filterOperator = mode == 'expired' ? '$lt' : '$gt';
+	// // Building the filters object
+	// const filters: any = { enrollmentDeadline: {} };
+	// filters.enrollmentDeadline[filterOperator] = today;
+
+	// // Requesting...
+	// return await request(
+	// 	fetchFn,
+	// 	`${b}api/courses?${query}`,
+	// 	'GET',
+	// 	null,
+	// 	headersAuth()
+	// );
 };
--- packages/apps/frontend/src/routes/courses/+page.svelte
+++ packages/apps/frontend/src/routes/courses/+page.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { TitleAndText, Container } from '$lib/components';
+	import { CourseCard } from '$lib/partials';
 
 	export let data: PageData;
+
+	const courses = data.data;
 </script>
 
-<Container padding>
+<Container padding direction="column">
 	<TitleAndText title="Courses" text="All the courses." />
+	<div class="pt-6 md:grid md:gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+		{#if courses && courses.length > 0}
+			{#each courses as course}
+				<CourseCard {course} />
+			{/each}
+		{:else}
+			<div>No courses found.</div>
+		{/if}
+	</div>
 </Container>
