005a6055821b76c1c11e311e460f8acdda171bab
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-11-25 14:27:15 +0100

    created 'courses' page

---
+++ packages/apps/frontend/src/routes/courses/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { TitleAndText, Container } from '$lib/components';
+
+	export let data: PageData;
+</script>
+
+<Container padding>
+	<TitleAndText title="Courses" text="All the courses." />
+</Container>
