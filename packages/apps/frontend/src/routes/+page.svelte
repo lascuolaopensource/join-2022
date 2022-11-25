b274278c9d078eb39998b4f97d8e6c9761704828
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-11-25 14:27:06 +0100

    update homepage

--- packages/apps/frontend/src/routes/+page.svelte
+++ packages/apps/frontend/src/routes/+page.svelte
@@ -1,12 +1,35 @@
 <script lang="ts">
 	import type { LayoutData } from './$types';
-	import { Heading } from 'flowbite-svelte';
-	import Container from '$lib/components/container.svelte';
+	import { Heading, Card, P } from 'flowbite-svelte';
+	import { Container, TitleAndText } from '$lib/components';
 
 	export let data: LayoutData;
+
+	let sections: Array<{ title: string; href: string; desc?: string; disabled?: boolean }> = [
+		{ title: 'Courses', href: '/courses', desc: 'Take a look at the new courses!' },
+		{
+			title: 'Tools & spaces',
+			href: '/',
+			desc: 'Book tools and rooms for a time slot for your projects',
+			disabled: true
+		}
+	];
 </script>
 
 <Container direction="column" padding>
-	<Heading tag="h2">Ciao {data.user?.info.name}!</Heading>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<TitleAndText title={`Ciao ${data.user?.info.name}!`} text="What do you want to do today?" />
+
+	<!-- List of sections -->
+	<div class="pt-6 flex flex-row flex-wrap space-x-4">
+		{#each sections as s}
+			<div class:opacity-40={s.disabled} class="basis-80">
+				<Card href={!s.disabled ? s.href : ''}>
+					<div class="space-y-2">
+						<Heading tag="h4">{s.title}</Heading>
+						<P>{s.desc}</P>
+					</div>
+				</Card>
+			</div>
+		{/each}
+	</div>
 </Container>
