8127cc1a459e1e5ab918e673bbddaa61a294aa29
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-11-28 10:14:55 +0100

    frontend: made course page public

--- packages/apps/frontend/src/routes/courses/[course]/+layout.ts
+++ packages/apps/frontend/src/routes/courses/[course]/+layout.ts
@@ -1,25 +1,12 @@
-import type { PageServerLoad } from './$types';
+import type { LayoutLoad } from './$types';
 import qs from 'qs';
-import { getJWT } from '$lib/utils/cookies';
 import { jr, Request, types as t } from 'join-shared';
 import type { LoadReturn } from '$lib/types';
 
 //
 
-export const load: PageServerLoad = async ({
-	params,
-	cookies,
-	fetch
-}): LoadReturn<t.CourseEntity> => {
-	const jwt = getJWT(cookies);
+export const load: LayoutLoad = async ({ params, fetch }): LoadReturn<t.CourseEntity> => {
 	const { course: slug } = params;
-
-	// This shouldn't happen, cause the user should be redirected to the login page before this
-	// But it's good to have this check here, for type safety
-	if (!jwt) {
-		console.log('no jwt');
-	}
-	console.log(slug);
 
 	// Creating filters
 	const query = qs.stringify({
@@ -36,7 +23,6 @@ export const load: PageServerLoad = async ({
 	const res = await jr.send<t.CourseEntityResponseCollection>({
 		fetchImpl: fetch,
 		path: `/courses?${query}`,
-		auth: jwt,
 		method: Request.HTTPMethod.GET
 	});
 
