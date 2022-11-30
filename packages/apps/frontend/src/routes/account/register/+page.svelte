0c2b41098b2291ee77fe5914a1a8798a5c1301cf
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-11-30 12:27:58 +0100

    fe: register: implemented new fields

--- packages/apps/frontend/src/routes/account/register/+page.svelte
+++ packages/apps/frontend/src/routes/account/register/+page.svelte
@@ -3,9 +3,10 @@
 	import paths from '$lib/constants/paths';
 	import { routes as r } from 'join-shared';
 
-	import { Input, Label, Button } from 'flowbite-svelte';
+	import { Input, Label } from 'flowbite-svelte';
+	import { TextField } from '$lib/form';
 	import { TitleAndLink } from '$lib/components';
-	import { Form } from '$lib/components/form';
+	import { Form } from '$lib/form';
 	//
 
 	export let form: ActionData;
@@ -30,49 +31,15 @@
 	validationSchema={r.Account.Register.schema}
 >
 	<div class="space-y-6">
-		<div>
-			<Label for="name">First name</Label>
-			<Input
-				name="name"
-				type="text"
-				id="name"
-				placeholder="Maria"
-				required
-				data-test="name"
-			/>
-		</div>
-		<div>
-			<Label for="surname">Last name</Label>
-			<Input
-				name="surname"
-				type="text"
-				id="surname"
-				placeholder="Rossi"
-				required
-				data-test="surname"
-			/>
-		</div>
-		<div>
-			<Label for="email">Email</Label>
-			<Input
-				name="email"
-				type="email"
-				id="email"
-				placeholder="maria@rossi.com"
-				required
-				data-test="email"
-			/>
-		</div>
-		<div>
-			<Label for="password">Password</Label>
-			<Input
-				name="password"
-				type="password"
-				id="password"
-				placeholder="********"
-				required
-				data-test="password"
-			/>
-		</div>
+		<TextField label="First name" name="name" type="text" placeholder="Maria" required />
+		<TextField label="Last name" name="surname" type="text" placeholder="Rossi" required />
+		<TextField label="Email" name="email" type="email" placeholder="maria@rossi.com" required />
+		<TextField
+			label="Password"
+			name="password"
+			type="password"
+			placeholder="********"
+			required
+		/>
 	</div>
 </Form>
