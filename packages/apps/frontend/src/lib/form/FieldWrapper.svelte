16fc0b60189a2e05835edd25c49c3ab51bac3cdb
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-12-01 11:40:49 +0100

    fe: fieldWrapper: cleaned logic

--- packages/apps/frontend/src/lib/form/FieldWrapper.svelte
+++ packages/apps/frontend/src/lib/form/FieldWrapper.svelte
@@ -2,7 +2,7 @@
 	import { getContext } from 'svelte';
 	import { key } from './key';
 	import type { FormState } from './types';
-	import { flatten } from 'flatten-anything';
+	import { flattenObject } from 'flatten-anything';
 
 	//
 
@@ -13,10 +13,10 @@
 	const { form, errors, handleChange } = getContext(key) as FormState;
 
 	let error: string | undefined;
-	$: error = (flatten($errors) as Record<string, any>)[name];
+	$: error = flattenObject($errors)[name];
 
 	let value: string | undefined;
-	$: value = (flatten($form) as Record<string, any>)[name];
+	$: value = flattenObject($form)[name];
 
 	let color: 'base' | 'green' | 'red' = 'base';
 	$: error ? (color = 'red') : (color = 'base');
