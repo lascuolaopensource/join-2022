77643e23dd9909cdfe749e12d4ff98d73bd16f61
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-03-16 20:26:17 +0100

    feat: completed transfer of old files

--- packages/apps/backend/config/database.ts
+++ packages/apps/backend/config/database.ts
@@ -1,11 +1,38 @@
-import path from 'path';
+import path from "path";
+
+function connectionDev(env) {
+    return {
+        client: "sqlite",
+        connection: {
+            filename: path.join(
+                __dirname,
+                "..",
+                "..",
+                env("DATABASE_FILENAME", "database/data.db")
+            ),
+        },
+        useNullAsDefault: true,
+    };
+}
+
+function connectionProd(env) {
+    return {
+        client: "mysql",
+        connection: {
+            host: env("DATABASE_HOST", "127.0.0.1"),
+            port: env.int("DATABASE_PORT", 3306),
+            database: env("DATABASE_NAME", "strapi"),
+            user: env("DATABASE_USERNAME", "strapi"),
+            password: env("DATABASE_PASSWORD", "strapi"),
+            ssl: false,
+        },
+        debug: false,
+    };
+}
 
 export default ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', 'database/data.db')),
-    },
-    useNullAsDefault: true,
-  },
+    connection:
+        env("DATABASE_ENV", "development") == "production"
+            ? connectionProd(env)
+            : connectionDev(env),
 });
