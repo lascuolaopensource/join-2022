8fd521de974cc58dc1b157233b3aea6ed55cffb9
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2023-03-21 12:44:55 +0100

    feat: updated sveltekit

--- packages/apps/frontend/README.md
+++
@@ -1,6 +0,0 @@
-## Component libraries
-
--   https://www.svelteui.org/
--   https://illright.github.io/attractions//docs/custom-elements
--   https://flowbite-svelte.com/
-    Provo questo perchè ha tailwind, mentre svelteUI, per quanto più completo, richiede un po' più di tempo per imparare
--- packages/apps/frontend/src/app.d.ts
+++ packages/apps/frontend/src/app.d.ts
@@ -1,11 +1,14 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-// and what to do when importing types
-declare namespace App {
-	interface Locals {
-		user: import('join-shared').routes.Account.Me.Res | null;
+declare global {
+	namespace App {
+		// interface Error {}
+		interface Locals {
+            user: import('join-shared').routes.Account.Me.Res | null;
+        }
+		// interface PageData {}
+		// interface Platform {}
 	}
-	// interface PageData {}
-	// interface Error {}
-	// interface Platform {}
 }
+
+export {};
